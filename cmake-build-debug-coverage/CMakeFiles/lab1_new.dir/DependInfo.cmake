b2beeb93a9d50b6508fdf570ded29d1d229acc47
Grisha <gkurennyy@mail.ru>
2021-02-22 23:12:55 +0300

    Create git

---
+++ cmake-build-debug-coverage/CMakeFiles/lab1_new.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/cc++ labs/lab1_new/complexNum.c" "C:/cc++ labs/lab1_new/cmake-build-debug-coverage/CMakeFiles/lab1_new.dir/complexNum.c.obj"
+  "C:/cc++ labs/lab1_new/main.c" "C:/cc++ labs/lab1_new/cmake-build-debug-coverage/CMakeFiles/lab1_new.dir/main.c.obj"
+  "C:/cc++ labs/lab1_new/menu.c" "C:/cc++ labs/lab1_new/cmake-build-debug-coverage/CMakeFiles/lab1_new.dir/menu.c.obj"
+  "C:/cc++ labs/lab1_new/myVector.c" "C:/cc++ labs/lab1_new/cmake-build-debug-coverage/CMakeFiles/lab1_new.dir/myVector.c.obj"
+  "C:/cc++ labs/lab1_new/stdMath.c" "C:/cc++ labs/lab1_new/cmake-build-debug-coverage/CMakeFiles/lab1_new.dir/stdMath.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/lab1_new.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_new.dir/complexNum.c.obj"
+  "CMakeFiles/lab1_new.dir/main.c.obj"
+  "CMakeFiles/lab1_new.dir/menu.c.obj"
+  "CMakeFiles/lab1_new.dir/myVector.c.obj"
+  "CMakeFiles/lab1_new.dir/stdMath.c.obj"
+  "lab1_new.exe"
+  "lab1_new.exe.manifest"
+  "lab1_new.pdb"
+  "liblab1_new.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab1_new.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
